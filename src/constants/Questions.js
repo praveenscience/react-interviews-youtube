ce745b44e7361462e96e73436e8a14b8e722027e
Praveen Kumar <praveenscience@gmail.com>
2020-12-13 11:35:23 +0000

    Created the question bank.

---
+++ src/constants/Questions.js
@@ -0,0 +1,22 @@
+export default {
+  ReactJS_Level1: [
+    "React JS Question 1 for Level 1",
+    "React JS Question 2 for Level 1",
+    "React JS Question 3 for Level 1"
+  ],
+  ReactJS_Level2: [
+    "React JS Question 1 for Level 2",
+    "React JS Question 2 for Level 2",
+    "React JS Question 3 for Level 2"
+  ],
+  NodeJS_Level1: [
+    "Node JS Question 1 for Level 1",
+    "Node JS Question 2 for Level 1",
+    "Node JS Question 3 for Level 1"
+  ],
+  NodeJS_Level2: [
+    "Node JS Question 1 for Level 2",
+    "Node JS Question 2 for Level 2",
+    "Node JS Question 3 for Level 2"
+  ]
+};
