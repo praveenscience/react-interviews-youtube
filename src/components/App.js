896e04fc41000380cc6a841e7845d28cd6f4d2fe
Praveen Kumar <praveenscience@gmail.com>
2020-12-13 11:44:44 +0000

    Converted Class Component to Functional App Component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./Header";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header className="Header" dark={true}>
-          React Interview Helper
-        </Header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Header className="Header" dark={true}>
+        React Interview Helper
+      </Header>
+    </div>
+  );
+};
+
+export default App;
