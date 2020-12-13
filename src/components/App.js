e89ee43664d8c8408c3e466b7a86b3bbb2870dad
Praveen Kumar <praveenscience@gmail.com>
2020-12-13 11:43:46 +0000

    Implemented a dark header.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 export default class App extends Component {
   render() {
-    return <div>Hello YouTube</div>;
+    return (
+      <div className="App">
+        <Header className="Header" dark={true}>
+          React Interview Helper
+        </Header>
+      </div>
+    );
   }
 }
